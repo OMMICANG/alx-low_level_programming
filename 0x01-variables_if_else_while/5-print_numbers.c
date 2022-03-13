047b9dc08c6aa3286311ce709eeb0bb3e29e7cf8
Michael Adebanjo <ommicang@gmail.com>
2022-03-13 18:55:56 +0000

    ...

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,9 @@
-
-   
 #include <stdio.h>
-
 /**
  * main - print single digit numbers
- *
  * Description: print single digit numbers
- *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	int i;
